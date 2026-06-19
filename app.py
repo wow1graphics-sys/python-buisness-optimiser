50ece6ac2d8fb8250b515170da0f61fbb3e5a608
pro-coder <wow1graphics@gmail.com>
2026-06-19 21:19:49 +0500

    Add files via upload

---
+++ app.py
@@ -0,0 +1,29 @@
+Python 3.14.6 (tags/v3.14.6:c63aec6, Jun 10 2026, 10:26:10) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> 
+============================================== RESTART: C:/Users/khan/AppData/Local/Programs/Python/Python314/app.py ==============================================
+Traceback (most recent call last):
+  File "C:/Users/khan/AppData/Local/Programs/Python/Python314/app.py", line 61, in <module>
+    if _name_ == "_main_":
+NameError: name '_name_' is not defined. Did you mean: '__name__'?
+>>> 
+============================================== RESTART: C:/Users/khan/AppData/Local/Programs/Python/Python314/app.py ==============================================
+==================================================
+      CORPORATE BUDGET & MARGIN OPTIMIZER       
+==================================================
+Enter your product or service name: shoes
+Enter selling price per unit of shoes ($): 20
+Enter variable cost per unit (materials, labor) ($): 12
+Enter total monthly fixed costs (rent, salaries, marketing) ($): 5
+Enter expected monthly units sold: 200
+
+--------------------------------------------------
+ FINANCIAL PERFORMANCE REPORT FOR: SHOES 
+--------------------------------------------------
+💰 Total Monthly Revenue:     $4,000.00
+📉 Total Monthly Expenses:    $2,405.00
+🟩 Net Monthly Profit:        $1,595.00
+📊 Net Profit Margin:         39.88%
+--------------------------------------------------
+✅ STATUS: PROFITABLE. You clear your break-even line of 0 units.
+==================================================
